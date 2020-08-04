103b5a14cf7ab165eba47de359d31370caf27ee6
pepeyen <erickfcaldeira@gmail.com>
2020-08-04 01:14:33 -0300

    Changed from BrowserRouter to HashRouter due to gh-pages compatibility

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 //Styles
 import './index.scss';
 
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 );
 
